9a78fd64f3f6cd7e3d8daf88157c47722b0d1937
Arin <arinyazilim@gmail.com>
2020-07-29 14:24:47 +0300

    "Ders 04"

--- src/index.js
+++ src/index.js
@@ -1,17 +1,60 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDOM from 'react-dom'
+
+
+// JSX
+
+// <button type="button" style="padding: 10px; color: white; background-color: red; border: 2px solid yellow;">HTML BUTTON</button>
+
+
+function App() {
+
+    const str1 = "Click ";
+    const str2 = "Me";
+
+    return (
+    <div>
+        <button type="button" style={{padding:'10px', color: 'white', backgroundColor: 'blue', border: '2px solid yellow'}}>{str1.concat(str2)}</button>  
+        
+    </div>
+    );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,
+    document.getElementById('root') 
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* const names = ["Arin", "Elis", "Gamze", "Gürcan"];
+
+function App() {
+  return /*#__PURE__React.createElement("div", { 
+    __self: this,
+  }, names.map(name => /*#__PURE__React.createElement("h1", {
+    __self: this,
+  }, name)));
+}
+
+ReactDOM.render( /*#__PURE__React.createElement(App, {
+  __self: undefined,
+}), document.getElementById('root'));  */
